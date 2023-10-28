f4836ce44afc6869343db2c2ac1a7d6aaf3ee629
Lars Möllendorf <lars@moellendorf.eu>
2023-10-28 20:26:25 +0200

    Explain what Average and Result values of Mindsensors Line Leader denotes

--- src/LmMsLineLeaderV2.h
+++ src/LmMsLineLeaderV2.h
@@ -5,6 +5,10 @@
  * @brief Mindsensors LineLeader V2 for Arduino
  *
  * @details
+ *
+ * The sensor's normal orientation is:
+ *
+ * Socket upward and port facing to the robot.
  */
 
 //! @cond SuppressGuard
@@ -42,7 +46,127 @@ class MsLineLeaderV2
 		int getBlackCalibration(char *values, size_t len);
 		int getVoltage(int *readings, size_t len);
 		int getSteering(void);
+
+		/**
+		 * Average:
+		 *
+		 * The average is a weighted average of the bits set to 1 based
+		 * on the position.
+		 * i.e. left most bit has weight of 10, second bit has weight
+		 * of 20.
+		 *
+		 * The diagrams below explains the line scenario and the
+		 * Average returned.
+		 *
+		 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		 * Suppose your line is narrow and only under the sensor 5.
+		 * I.e. all the 8 sensors except the 5th sensor will return
+		 * zero value. And the 5 th bit will be 1.
+		 *
+		 * <pre>
+		 *                             XXXXX
+		 *                             XXXXX
+		 *             +---+---+---+---+---+---+---+---+
+		 *             | W | W | W | W | B | W | W | W |
+		 *             +---+---+---+---+---+---+---+---+
+		 *                             XXXXX
+		 *                             XXXXX
+		 * </pre>
+		 *
+		 * The ‘Average’ returned in this case
+		 * will be 50, calculated as: (0+0+0+0+50+0+0+0)/1
+		 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		 * Suppose, your line is wider, and 4 sensors are over the
+		 * line.
+		 *
+		 * <pre>
+		 *                     XXXXXXXXXXXXXXXXX
+		 *                     XXXXXXXXXXXXXXXXX
+		 *             +---+---+---+---+---+---+---+---+
+		 *             | W | W | B | B | B | B | W | W |
+		 *             +---+---+---+---+---+---+---+---+
+		 *                     XXXXXXXXXXXXXXXXX
+		 *                     XXXXXXXXXXXXXXXXX
+		 * </pre>
+		 *
+		 * The ‘Average’ returned in this case will be 45, calculated
+		 * as: (0+0+30+40+50+60+0+0)/4
+		 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		 * <pre>
+		 *                                         XXXXX
+		 *                                         XXXXX
+		 *             +---+---+---+---+---+---+---+---+
+		 *             | W | W | W | W | W | W | W | B |
+		 *             +---+---+---+---+---+---+---+---+
+		 *                                         XXXXX
+		 *                                         XXXXX
+		 * </pre>
+		 *
+		 * The ‘Average’ returned in this case will be 80, calculated
+		 * as: (0+0+0+0+0+0+0+80)/1
+		 *
+		 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		 */
 		int getAverage(void);
+
+		/**
+		 * Result:
+		 *
+		 * Result is a byte value of the sensor reading. Each bit
+		 * corresponding to the sensor where the line is seen is set to
+		 * 1, or else the bit is zero.
+		 *
+		 * The diagrams below explains the line scenario and the
+		 * Result returned.
+		 *
+		 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		 * Suppose your line is narrow and only under the sensor 5.
+		 * I.e. all the 8 sensors except the 5th sensor will return
+		 * zero value. And the 5 th bit will be 1.
+		 *
+		 * <pre>
+		 *                             XXXXX
+		 *                             XXXXX
+		 *             +---+---+---+---+---+---+---+---+
+		 *             | W | W | W | W | B | W | W | W |
+		 *             +---+---+---+---+---+---+---+---+
+		 *                             XXXXX
+		 *                             XXXXX
+		 * </pre>
+		 *
+		 * The ‘Result’ returned in this case will be binary:
+		 * 0b00001000 (or decimal: 8 or hex: 0x08).
+		 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		 * Suppose, your line is wider, and 4 sensors are over the
+		 * line.
+		 *
+		 * <pre>
+		 *                     XXXXXXXXXXXXXXXXX
+		 *                     XXXXXXXXXXXXXXXXX
+		 *             +---+---+---+---+---+---+---+---+
+		 *             | W | W | B | B | B | B | W | W |
+		 *             +---+---+---+---+---+---+---+---+
+		 *                     XXXXXXXXXXXXXXXXX
+		 *                     XXXXXXXXXXXXXXXXX
+		 * </pre>
+		 *
+		 * The Result returned in this case will be binary: 0b00111100
+		 * (or decimal: 60 or hex: 0x3C)
+		 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		 * <pre>
+		 *                                         XXXXX
+		 *                                         XXXXX
+		 *             +---+---+---+---+---+---+---+---+
+		 *             | W | W | W | W | W | W | W | B |
+		 *             +---+---+---+---+---+---+---+---+
+		 *                                         XXXXX
+		 *                                         XXXXX
+		 * </pre>
+		 *
+		 * The ‘Result’ returned in this case will be binary:
+		 * 0b00000001 (or decimal: 1 or hex: 0x01).
+		 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		 */
 		int getResult(void);
 		int getSetpoint(void);
 		int setSetpoint(char setpoint);
